049b6901b4dfed7d720c36f1e9340d54b47a5642
mpostument <777rip777@gmail.com>
2020-05-16 18:19:03 +0300

    initial commit

---
+++ CsvLibrary/Csv.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace CsvLibrary
+{
+    public class Csv
+    {
+        public void Write(string path, IEnumerable<object> records)
+        {
+            using var writer = new StreamWriter(path);
+            using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+            csv.WriteRecords(records);
+        }
+
+        public List<dynamic> Read(string path)
+        {
+            using var reader = new StreamReader(path);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var result = csv.GetRecords<dynamic>().ToList();
+            return result;
+        }
+    }
+}
---
+++ awstaghelper/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using Services;
+using CsvLibrary;
+
+namespace awstaghelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Csv csv = new Csv();
+            Ec2 ec2 = new Ec2(csv);
+            ec2.ReadCsv();
+        }
+    }
+}
