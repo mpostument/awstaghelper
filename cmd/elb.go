c6f1f156a1e10a5ad7b69fbb905066633917dee3
Maksym Postument <777rip777@gmail.com>
2020-08-10 19:15:57 +0300

    Added elbv2 support (#9)

---
+++ cmd/elb.go
@@ -0,0 +1,72 @@
+/*
+Copyright © 2020 Maksym Postument 777rip777@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"awstaghelper/libs/commonLib"
+	"awstaghelper/libs/elbLib"
+	"github.com/aws/aws-sdk-go/service/elbv2"
+	"github.com/spf13/cobra"
+)
+
+// elbCmd represents the elb command
+var elbCmd = &cobra.Command{
+	Use:   "elb",
+	Short: "Root command for interaction with AWS elb services",
+	Long:  `Root command for interaction with AWS elb services.`,
+	//Run: func(cmd *cobra.Command, args []string) {
+	//	fmt.Println("elb called")
+	//},
+}
+
+var getElbV2Cmd = &cobra.Command{
+	Use:   "get-elbv2-tags",
+	Short: "Write elbv2 arn and required tags to csv",
+	Long: `Write to csv data with elbv2 arn and required tags to csv. 
+This csv can be used with tag-elbv2 command to tag aws environment.
+Specify list of tags which should be read using tags flag: --tags Name,Env,Project.
+Csv filename can be specified with flag filename.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		tags, _ := cmd.Flags().GetString("tags")
+		filename, _ := cmd.Flags().GetString("filename")
+		profile, _ := cmd.Flags().GetString("profile")
+		region, _ := cmd.Flags().GetString("region")
+		sess := commonLib.GetSession(region, profile)
+		client := elbv2.New(sess)
+		commonLib.WriteCsv(elbLib.ParseElbV2Tags(tags, client), filename)
+	},
+}
+
+var tagElbV2Cmd = &cobra.Command{
+	Use:   "tag-elbv2",
+	Short: "Read csv and tag elbv2 with csv data",
+	Long:  `Read csv generated with get-elbv2-tags command and tag elbv2 instances with tags from csv.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		filename, _ := cmd.Flags().GetString("filename")
+		profile, _ := cmd.Flags().GetString("profile")
+		region, _ := cmd.Flags().GetString("region")
+		sess := commonLib.GetSession(region, profile)
+		client := elbv2.New(sess)
+		csvData := commonLib.ReadCsv(filename)
+		elbLib.TagElbV2(csvData, client)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(elbCmd)
+	elbCmd.AddCommand(getElbV2Cmd)
+	elbCmd.AddCommand(tagElbV2Cmd)
+}
---
+++ libs/elbLib/getElbV2Tags.go
@@ -0,0 +1,55 @@
+package elbLib
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/service/elbv2"
+	"github.com/aws/aws-sdk-go/service/elbv2/elbv2iface"
+	"log"
+	"strings"
+)
+
+// getElbV2 return all elbv2 (application and network) instances from specified region
+func getElbV2(client elbv2iface.ELBV2API) []*elbv2.LoadBalancer {
+	input := &elbv2.DescribeLoadBalancersInput{}
+
+	var result []*elbv2.LoadBalancer
+
+	err := client.DescribeLoadBalancersPages(input,
+		func(page *elbv2.DescribeLoadBalancersOutput, lastPage bool) bool {
+			result = append(result, page.LoadBalancers...)
+			return !lastPage
+		})
+	if err != nil {
+		log.Fatal("Not able to get load balancer", err)
+		return nil
+	}
+	return result
+}
+
+// ParseElbV2Tags parse output from getInstances and return instances id and specified tags.
+func ParseElbV2Tags(tagsToRead string, client elbv2iface.ELBV2API) [][]string {
+	instancesOutput := getElbV2(client)
+	var rows [][]string
+	headers := []string{"Arn"}
+	headers = append(headers, strings.Split(tagsToRead, ",")...)
+	rows = append(rows, headers)
+	for _, elb := range instancesOutput {
+		elbTags, err := client.DescribeTags(&elbv2.DescribeTagsInput{ResourceArns: []*string{elb.LoadBalancerArn}})
+		if err != nil {
+			fmt.Println("Not able to get lambda tags", err)
+		}
+		tags := map[string]string{}
+		for _, tagsToWrite := range elbTags.TagDescriptions {
+			for _, tag := range tagsToWrite.Tags {
+				tags[*tag.Key] = *tag.Value
+			}
+		}
+
+		var resultTags []string
+		for _, key := range strings.Split(tagsToRead, ",") {
+			resultTags = append(resultTags, tags[key])
+		}
+		rows = append(rows, append([]string{*elb.LoadBalancerArn}, resultTags...))
+	}
+	return rows
+}
